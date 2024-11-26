c514c6e3b28d85e936ffcd43e4d541ac62a0fb2b
Mario Gonzalez <96237810+Brutus181@users.noreply.github.com>
2024-11-26 12:42:40 +0100

    Añadido modelo de wombi y sus animaciones
    Falta ajustar el arbol de estados pero actualmente no tengo ninguna idea de como son los controles del juego

---
+++ My project/Assets/Asset3D/Modelos/WOMBI/wombi_animation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wombi_animation : MonoBehaviour
+{
+    private Animator anim;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            anim.SetTrigger("Push");
+        } else if (Input.GetKeyDown(KeyCode.T))
+        {
+            anim.SetTrigger("Attack");
+        } else if (Input.GetKeyDown(KeyCode.R))
+        {
+            anim.SetTrigger("Walk");
+        }
+    }
+    
+    
+    
+}
